92eef7664e6283a87a7e689180366fbb726c5a12
Rhian <rhian.gwawr@gmail.com>
2018-12-23 22:03:27 +0000

    More tidying up of UI

--- src/app/sessions/completed/exercise/exercise.component.ts
+++ src/app/sessions/completed/exercise/exercise.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, OnInit, OnChanges } from "@angular/core";
 import { ICompletedExercise } from "src/app/shared/interfaces/completed-exercise";
+import { ISet } from "src/app/shared/interfaces/set";
 
 
 @Component({
@@ -7,12 +8,22 @@ import { ICompletedExercise } from "src/app/shared/interfaces/completed-exercise
     templateUrl: 'exercise.component.html',
     styleUrls: ['exercise.component.css']
 })
-export class CompletedExerciseComponent {
+export class CompletedExerciseComponent implements OnInit, OnChanges {
     @Input() exercise: ICompletedExercise;
     @Input() showWarmups: boolean;
     collapsed: boolean = false;
+    warmups: ISet[];
 
     ngOnInit(): void {
+        this.updateWarmups();
+    }
+
+    ngOnChanges(): void {
+        this.updateWarmups();
+    }
+
+    updateWarmups() {
+        this.warmups = this.showWarmups ? this.exercise.warmup : [];
     }
 
     toggleCollapsed(): void {
